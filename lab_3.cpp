39199ea4f8b06a4c49db774d2b87a7b97c3552ee
kirill89450 <49889375+kirill89450@users.noreply.github.com>
2020-05-18 10:33:49 +0700

    +2 code file

---
+++ lab_3.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+string get_word(istream& is) {
+    string w;
+    is >> w;
+    return w;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    ifstream file("text.txt");
+    if (!file) {
+        cerr << "Не удалось открыть файл" << endl;
+        return 1;
+    }
+
+    string word, max_word;
+    size_t len, max_len = 0;
+
+    while (file) {
+        word = get_word(file);
+        if (word.length() == 0)
+            break;
+        len = word.length();
+        if (len > max_len) {
+            max_len = len;
+            max_word = word;
+        }
+        
+    }
+
+    file.close();
+
+    cout << "Самое длинное слово:  \"" << max_word << "\"" << endl;
+
+    return 0;
+}
