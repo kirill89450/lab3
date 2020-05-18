1788dd448611b942dffd581495ae102795f338c9
kirill89450 <49889375+kirill89450@users.noreply.github.com>
2020-05-18 17:47:05 +0700

    Доработка версия номер 0.1

--- lab3.1.cpp
+++ lab3.1.cpp
@@ -1,22 +1,38 @@
-﻿// lab3.1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
 
-#include <iostream>
 using namespace std;
+
+int getSize(char* string) {
+    int size = 0;
+    int i = 0;
+    while (string[i] != '\0') {
+        size++;
+        i++;
+    }
+    return size;
+}
+char* concatination(char* first, char* second, int a, int b) {
+    int newSize = a + b;
+    char* s = (char*)calloc(newSize, sizeof(char));
+    int i = 0;
+    for (i = 0; i < a; i++) s[i] = first[i];
+    for (int j = 0; j < b; j++) s[i + j] = second[j];
+    return s;
+}
 int main()
 {
     setlocale(LC_ALL, "Russian");
-    char s1[] = "привет";
-    char s2[] = "мир";
-    cout <<"Имеются строки"<<endl<< s1 << endl << s2 << endl;
-    int size = 12;
-    char* s3 = (char*)calloc(size, sizeof(char));
-    int i = 0;
-    for (i = 0; i < 7; i++) s3[i] = s1[i];
-    for (int j = 0; j < 4; j++) s3[i + j] = s2[j];
-    cout << "Их конкатенация= ";
-    for (int k = 0; k < 12; k++) cout << s3[k];
+    char s1[100], s2[100];
+    cout << "Введите 1 строку: " << endl;
+    cin >> s1;
+    cout << "Введите 2 строку: " << endl;
+    cin >> s2;
+    cout << "Строки: " << endl << s1 << endl << s2 << endl;
+    int newSize = getSize(s1) + getSize(s2);
+    char* s = concatination(s1, s2, getSize(s1), getSize(s2));
+    cout << "Конкатенация: ";
+    for (int k = 0; k < newSize; k++) cout << s[k];
+
+
     return 0;
 }
-asd
-
